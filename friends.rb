97bcdc4a4651b21721f2baa3c9bc7ffef8d57083
Billy Burch <billyburch40@gmail.com>
2023-08-02 14:27:25 -0600

    Add places to go

--- friends.rb
+++ friends.rb
@@ -2,3 +2,6 @@ friends = ["Jack", "Jill", "HumptyDumpty", "Matt", "Jason"]
 p friends[2]
 p friends[4]
 p friends[0]
+
+places = ["del norte", "alamosa", "south fork", "monte vista", "crested butte"]
+places.each { |p| puts "I'm going to #{p}" }
